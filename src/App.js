c306b281d9c4d536c12a4da50a616993a7bba614
ocean-p <duongkodoiqua@gmail.com>
2023-02-14 16:56:48 +0700

    add components and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import MovieDetail from './components/MovieDetail/MovieDetail'
+import PageNotFound from './components/PageNotFound/PageNotFound'
+import Footer from './components/Footer/Footer'
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/movie/:imdbID' element={<MovieDetail/>} />
+          <Route path='/*' element={<PageNotFound/>} />
+        </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
 }
---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = '4daeaed3'
