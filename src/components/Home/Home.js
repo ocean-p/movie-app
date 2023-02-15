92b92930a5e8728d9b8ea4fd1bdd0f5f7ced9f5a
ocean-p <duongkodoiqua@gmail.com>
2023-02-15 16:07:08 +0700

    get list of movies and show them

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import MovieListing from '../MovieListing/MovieListing'
+import movieApi from '../../common/apis/movieApi'
+import {APIKey} from '../../common/apis/MovieApiKey'
+import { useDispatch } from 'react-redux'
+import { addMovies } from '../../features/movies/movieSlice'
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const movieText = 'Demon Slayer'
+    const fetchMovies = async () => {
+      const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch(err => {
+          console.log('Err: ', err)
+        })
+      console.log(response)
+      dispatch(addMovies(response.data))
+    }
+    fetchMovies();
+  }, [dispatch])
+
   return (
-    <div>Home</div>
+    <div>
+      <div className='banner-img'></div>
+      <MovieListing/>
+    </div>
   )
 }
 
